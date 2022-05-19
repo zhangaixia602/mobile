e139b5e67ce25d43e00fd2adb020d970ed9981c8
zhangaixia602 <zhangaixia602@163.com>
2022-05-19 17:55:32 +0800

    添加案例模块

---
+++ src/components/CasePage.vue
@@ -0,0 +1,171 @@
+<template>
+	<section>
+		<h3 class="titbg">案例<span> / cases</span></h3>
+		<div class='nav'>
+			<div class='navItem' v-for='(nav,index) in navs' :key='index' @click="showNav(nav,index)"
+				:class="{active: this.currentIndex===index}">
+				<i class="icon iconfont" :class="nav.icon"></i>
+				<p>{{nav.title}}</p>
+			</div>
+		</div>
+		<div class='product'>
+			<div class='wrapper'>
+				<a v-for='(product,index) in products' :key='index' @click="showPage(product)"
+					class='productItem' :class="{isShow: this.currentgroup===product.group || this.currentgroup===''}">
+					<img :src="require('../' + product.src)" />
+					<h3>{{product.title}}</h3>
+				</a>
+			</div>
+		</div>
+	</section>
+</template>
+<script>
+	import {
+		defineComponent
+	} from "vue";
+	export default defineComponent({
+		name: 'CasePage',
+		data() {
+			return {
+				currentIndex: 0,
+				currentgroup: '',
+				navs: [{
+						title: '全部',
+						icon: 'icon-zhihuiyuanqu',
+						group: ''
+					},
+					{
+						title: '园区',
+						icon: 'icon-zhihuiyuanqu',
+						group: 'park'
+					},
+					{
+						title: '水利',
+						icon: 'icon-shuiliqiye',
+						group: 'draulic'
+					},
+					{
+						title: '能源',
+						icon: 'icon-zhihuiyuanqu',
+						group: 'energy'
+					}
+				],
+				products: [{
+						group: 'park',
+						title: '智慧工厂',
+						src: 'assets/yq.png',
+						id: '01',
+						path: '/model/insect'
+					},
+					{
+						group: 'energy',
+						title: '智慧能源',
+						src: 'assets/ny.png',
+						id: '02',
+						path: '/model/granary'
+					},
+					{
+						group: 'park',
+						title: '智慧粮仓',
+						src: 'assets/lc.png',
+						id: '03',
+						path: '/model/factory'
+					},
+					{
+						group: 'draulic',
+						title: '智慧水利',
+						src: 'assets/water.png',
+						id: '05',
+						path: '/model/water'
+					},
+					{
+						group: 'park',
+						title: '智慧厂区',
+						src: 'assets/cyq.png',
+						id: '05',
+						path: '/model/earth'
+					}
+
+				]
+			}
+		},
+		methods: {
+			showNav(nav, index) {
+				this.currentgroup = nav.group
+				this.currentIndex = index
+			},
+			showPage(product){
+				console.log(product)
+			}
+		}
+	})
+</script>
+<style scoped>
+	.nav {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		padding: 1.5rem 1rem;
+	}
+
+	.navItem {
+		display: inline-block;
+		font-size: 0.7rem;
+		color: #484848;
+		border: 0.1rem solid #484848;
+		width: 3rem;
+		height: 3rem;
+		border-radius: 0.5rem;
+		text-align: center;
+	}
+
+	.navItem .icon {
+		font-size: 1.1rem;
+		font-weight: bold;
+	}
+
+	.navItem.active {
+		color: #006ebf;
+		border-color: #006ebf;
+	}
+
+	.product {
+		padding:1rem;
+		background: white;
+	}
+
+	.product .wrapper {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		cursor: pointer;
+	}
+
+	.productItem {
+		width: 5rem;
+		display: none;
+		border: 1px solid #edf1f7;
+		box-shadow: 0 0.2rem 0.3rem 0 #edf1f7;
+		border-radius: 0.3rem;
+		margin-bottom: 1rem;
+		position: relative;
+	}
+
+	.productItem.isShow {
+		display: block;
+	}
+
+	.productItem img {
+		width: 100%;
+		height: 4rem;
+		filter: blur(0);
+	}
+
+	.productItem h3 {
+		font-size: 0.7rem;
+		color: #0d0e0f;
+		padding: 0.5rem;
+		margin: 0;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import router from './router';
+import './assets/css/common.css';
+import './assets/font/iconfont.css';
 import App from './App.vue'
-createApp(App).use(Antd).mount("#app");
+createApp(App).use(Antd).use(router).mount("#app");
