20643f14a29d65e3cfef06c886183e5f22de8b75
cwt1234 <974338217@qq.com>
2022-05-20 18:14:41 +0800

    提交

--- src/components/MiddlePage.vue
+++ src/components/MiddlePage.vue
@@ -10,7 +10,22 @@
 			<p style="line-height:25px;font-size: 12px;">{{ banner.text }}</p>
 		</div>
 	    </a-carousel>
-		</div>		
+		</div>	
+		<h3 class="titbg">可视化制作流程</h3>	
+		<a-tabs tabPosition="top" v-model:activeKey="activeKey" centered >
+        <a-tab-pane v-for="(tab, index) in tabs" :key="index">
+          <template #tab>
+            <div class="tItem">
+              {{ tab.title }}
+              <i class="icon iconfont" :class="tab.icon"></i>
+            </div>
+          </template>
+          <div class="tabItem">
+            <img :src="require('../' + tab.src)" />
+            <p>{{ tab.content }}</p>
+          </div>
+        </a-tab-pane>
+      </a-tabs>
 	</section>
 </template>
 <script>
@@ -39,6 +54,36 @@
 						text: "通过大量物联网设备数据采集、清洗、加工、存储等基本数据治理后,基于业务规则、数据标准将非同源的数据横向拉通构建完整统一智慧粮仓系统利用三维视图结合虚拟现实技术，将包括站坪、建筑物、粮仓。",
 					}
 				],
+				tabs: [
+        {
+          title: "数据模型采集",
+        //   icon: "icon-gengduo",
+          src: "assets/cj.png",
+          content:
+            "现如今园区、景区、工厂涉及的信息要素很多，对数据模型采集我们根据不同方式方法对模型进行真实性数据采集，达到可支持1:1还原建筑设备的制作基础。真实照片采集+CAD图纸，SCARDA采集数据。",
+        },
+        {
+          title: "模型制作",
+        //   icon: "icon-gengduo",
+          src: "assets/model.png",
+          content:
+            "我们通过三维建模工具，例如：3DMAX、Blender等，对模型的点、线、面、 贴图位置进行构建。模型构建基础形状设备设施,模型细节精度提升建设,模型贴图纹理还原真实,模型材质粗糙反射还原真实,模型效果渲染调试。",
+        },
+        {
+          title: "三维可视化功能",
+        //   icon: "icon-gengduo",
+          src: "assets/3D.png",
+          content:
+            "基于WebGL标准自主封装框架KT 3D-SDK进行系统三维可视化功能编程，实现所看即所得。实现与真实设备的交互反控,实现与真实场景无缝衔接切换,实现数据高速统计分析。",
+        },
+        {
+          title: "大数据集成",
+        //   icon: "icon-jiantou_xiangxia",
+          src: "assets/dsj.png",
+          content:
+            "采用新一代HTML5+WebGL构架和Canvas布局的B/S 架构，电脑不 用安装任何插件软件即可运行。大数据图表可视化,图形分析化能力,交互式观察数据改变,一键集成方便部署。",
+        },
+      ],
 			}
 			
 		},
@@ -52,6 +97,31 @@
       background-color:#f5f7fa;
       border-radius: 10px;
 	}
+.tItem{
+	font-size: 12px;
+}
+.tabItem {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  /* justify-content: space-around; */
+  height: 16rem;
+  background-color: #f5f7fa;
+  border-radius: 10px;
+}
+.tabItem img{
+    width: 90%;
+    height: 10rem;
+    padding-left: 20;
+    padding: 0.3rem 0;
+}
+.tabItem p{   
+    padding: 0rem 1rem;
+    font-size: 12px;
+    color: #2d2e33;
+    text-indent: 2em;
+    line-height: 1.5rem;
+}
 
 
 </style>
